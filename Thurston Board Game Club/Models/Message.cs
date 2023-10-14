a7b7920d1e1daf456f39ff1e58baa228ccdb5e4f
OddPrograms <OddPrograms@gmail.com>
2023-10-13 21:48:32 -0700

    Added Priority.

--- Thurston Board Game Club/Models/Message.cs
+++ Thurston Board Game Club/Models/Message.cs
@@ -5,6 +5,7 @@
         public string To { get; set; }
         public string From { get; set; }
         public string Text { get; set; }
+        public int Priority { get; set; }
         public DateTime Date { get; set; }
     }
 }
