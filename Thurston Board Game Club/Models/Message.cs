61b907fa4ba5bdb4b002867cabbd4ab576197f59
FrostPLane <114699910+FrostPLane@users.noreply.github.com>
2023-10-16 14:06:19 -0700

    Add files via upload
    Added subject line.

--- Thurston Board Game Club/Models/Message.cs
+++ Thurston Board Game Club/Models/Message.cs
@@ -4,6 +4,7 @@
     {
         public string To { get; set; }
         public string From { get; set; }
+        public string Subject { get; set; }
         public string Text { get; set; }
         public int Priority { get; set; }
         public DateTime Date { get; set; }
