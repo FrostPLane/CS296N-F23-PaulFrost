e390684217c11d7045fb4909d58e79dc2983b7ae
OddPrograms <OddPrograms@gmail.com>
2023-11-02 17:43:08 -0700

    Added quiz and unit testing for quiz.

---
+++ Thurston Board Game Club/Models/QuizQuestions.cs
@@ -0,0 +1,11 @@
+﻿namespace Thurston_Board_Game_Club.Models
+{
+    public class QuizQuestions
+
+    {
+        public Dictionary<int, String> Questions { get; set; }
+        public Dictionary<int, String> Answers { get; set; }
+        public Dictionary<int, String> InputAnswers { get; set; }
+        public Dictionary<int, bool> Results { get; set; } // result of checking the answers
+    }
+}
---
+++ Thurston Board Game Club/Views/Quiz/Quiz.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Thurston_Board_Game_Club.Views.Quiz
+{
+    public class QuizModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
