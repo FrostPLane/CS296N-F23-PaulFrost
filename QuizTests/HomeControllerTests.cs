dde9d8a32904c711c335440eda406acd618974f2
OddPrograms <OddPrograms@gmail.com>
2024-02-11 15:22:39 -0800

    Updating async testing.

--- QuizTests/HomeControllerTests.cs
+++ QuizTests/HomeControllerTests.cs
@@ -6,21 +6,19 @@ using Microsoft.AspNetCore.Routing;
 using System;
 using Xunit;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Message = ThurstonBoardGameClub.Models.Message;
 
 namespace ThurstonBoardGameClub.Controllers
 {
     public class MessageControllerTests
     {
         IMessageRepository repo = new FakeMessageRepository();
-        private readonly ILogger<HomeController> logger;
-        private readonly UserManager<AppUser> userm;
-
         HomeController controller;
-
+        
         public MessageControllerTests()
         {
-            controller = new HomeController(repo, logger, userm);
+            controller = new HomeController(repo, null, null);
         }
 
         [Fact]
@@ -50,5 +48,62 @@ namespace ThurstonBoardGameClub.Controllers
             // Check to see if I got a RedirectToActionResult
             Assert.True(result.GetType() == typeof(ViewResult));
         }
+
+        [Fact]
+        public void FromQueryTest()
+        {
+            var message1 = new Message() {  };
+            repo.StoreMessageAsync(message1).Wait();
+            repo.StoreMessageAsync(message1).Wait();
+            var message2 = new Message() {  };
+            repo.StoreMessageAsync(message2).Wait();
+            repo.StoreMessageAsync(message2).Wait();
+            var message3 = new Message() { From = "From 3" };
+            repo.StoreMessageAsync(message3).Wait();
+            repo.StoreMessageAsync(message3).Wait();
+
+            var controller = new HomeController(repo, null, null);
+
+            // Act
+            var filteredMessagesView = controller.MessageBoard(message2.From, null).Result as ViewResult;
+            List<Message> filteredReviews = filteredMessagesView.Model as List<Message>;
+
+            // Assert
+            Assert.Equal(2, filteredReviews.Count);
+            Assert.Equal(filteredReviews[0].From, message2.From);
+            Assert.Equal(filteredReviews[1].From, message2.From);
+        }
+
+        [Fact]
+        public void DateQueryTest()
+        {
+            // Test to see if only reviews with the selected title are returned 
+
+            // Arrange
+            // Done in the constructor
+
+            // We don't need need to add all the properties to the models since we aren't testing that.
+            var message1 = new Message() { Date = DateTime.Parse("01/01/2020") };
+            repo.StoreMessageAsync(message1);
+            repo.StoreMessageAsync(message1);
+            var message2 = new Message() { Date = DateTime.Parse("06/15/2021") };
+            repo.StoreMessageAsync(message2);
+            repo.StoreMessageAsync(message2);
+            var message3 = new Message() { Date = DateTime.Parse("12/31/2022") };
+            repo.StoreMessageAsync(message3);
+            repo.StoreMessageAsync(message3);
+
+            var controller = new HomeController(repo, null, null);
+
+            // Act
+            var filteredReviewsView = controller.MessageBoard(null, message2.Date.ToShortDateString()).Result as ViewResult;
+            List<Message> filteredReviews = filteredReviewsView.Model as List<Message>;
+
+            // Assert
+            Assert.Equal(2, filteredReviews.Count);
+            Assert.Equal(filteredReviews[0].Date, message2.Date);
+            Assert.Equal(filteredReviews[1].Date, message2.Date);
+        }
+
     }
 }
